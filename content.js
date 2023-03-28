f41fb758eef012056ead0f315415189410ff3cd4
radimbig <radimvadim@gmail.com>
2023-03-28 23:00:08 +0200

    chess detector

--- content.js
+++ content.js
@@ -1,1 +1,14 @@
-alert("You are on lichess.org")
+function MainBoardDetector() {
+  const MainBoard = document.querySelector(".round__app__board.main-board");
+  if (!MainBoard) {
+    return;
+  }
+  alert("Main board was detected")
+  const chessTable = MainBoard.querySelector("cg-container");
+  if(!chessTable){
+    return;
+  }
+  alert(`Chess table was detected style: width ${chessTable.style.width}  height ${chessTable.style.height}`);
+}
+
+window.onload = MainBoardDetector;
--- popup.js
+++
@@ -1,1 +0,0 @@
-alert("You activate popup of extensions");
