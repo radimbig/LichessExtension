cce5783b6de8cb710eb153903cb15d193d866750
Radimbig <radimvadim@gmail.com>
2023-04-01 19:45:31 +0300

    global varibles + background.js

--- content.js
+++ content.js
@@ -1,14 +1,58 @@
-function MainBoardDetector() {
+// global variables
+let IsTokenProvided;
+let token;
+const LichessTokenKey = "Lichess-Token";
+const IsTokenProvidedKey = "IsTokenProvided";
+// html elements
+let body;
+let inputToken;
+let SubmitButton;
+let winRange;
+let digitalRepresentationt;
+
+
+
+
+
+
+const MyStorage ={
+  Get:async function (key) {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get([key], function(result) {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(result[key]);
+        }
+      });
+    });
+  },
+  Set:async function(key, value) {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.set({ [key]: value }, function() {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+}
+
+
+async function MainBoardDetector() {
+  alert(await MyStorage.Get(LichessTokenKey));
   const MainBoard = document.querySelector(".round__app__board.main-board");
   if (!MainBoard) {
     return;
   }
-  alert("Main board was detected")
+  // alert("Main board was detected")
   const chessTable = MainBoard.querySelector("cg-container");
   if(!chessTable){
     return;
   }
-  alert(`Chess table was detected style: width ${chessTable.style.width}  height ${chessTable.style.height}`);
+  //  alert(`Chess table was detected style: width ${chessTable.style.width}  height ${chessTable.style.height}`);
 }
 
 window.onload = MainBoardDetector;
--- popup.js
+++ popup.js
@@ -1,13 +1,3 @@
-// global variables
-let IsTokenProvided;
-let token;
-
-// html elements
-let body;
-let inputToken;
-let SubmitButton;
-let winRange;
-let digitalRepresentationt;
 
 
 /*
@@ -18,35 +8,16 @@ let digitalRepresentationt;
 document.addEventListener("DOMContentLoaded", init);
 
 // functions
-function init() {
+ async function init() {
   // token checker
   
-  body = document.getElementById("root");
-  binder();
   
-  chrome.storage.local.get("IsTokenProvided", function (result) {
-    if (result["IsTokenProvided"] == undefined) {
-      chrome.storage.local.set({ "IsTokenProvided": false });
-      return;
-    }
-    IsTokenProvided = result["IsTokenProvided"];
-    chrome.storage.local.get("Lichess-Token", function (result) {
-      if (result["Lichess-Token"] == undefined) {
-        chrome.storage.local.set({ "Lichess-Token": "" });
-        return;
-      }
-      alert(result["Lichess-Token"]);
-      token = result["Lichess-Token"];
-    });
-  });
-// elements injection
-//   body.appendChild(inputToken);
-//   body.appendChild(SubmitButton);
-//   body.appendChild(winRange);
+  binder();
+  token = await MyStorage.Get("Lichess-Token");
+  if(token == undefined){
+    alert("Provide a token!");
+  }
 
-
-// win input
-inputToken.value = token;
 
 
 }
@@ -54,24 +25,33 @@ inputToken.value = token;
 
 
 function binder(){
+  
+  body = document.getElementById("root");
   digitalRepresentationt = document.getElementById("digitalRepresentationt");
   inputToken = document.getElementById("token");
   SubmitButton = document.getElementById("submit");
   winRange = document.getElementById("winRange");
-  document.getElementById("pin").onclick = pin;
-  document.getElementById("popup").onclick = popUp;
-  document.getElementById("otherTab").onclick = otherTab;
+  
+  // disable winrange
   winRange.addEventListener('mousedown', (event) => event.preventDefault());
   winRange.addEventListener('touchstart', (event) => event.preventDefault());
   winRange.addEventListener('keydown', (event) => event.preventDefault());
+  // listeners
+  document.getElementById("pin").onclick = pin;
+  document.getElementById("popup").onclick = popUp;
+  document.getElementById("otherTab").onclick = otherTab;
   SubmitButton.onclick = setToken;
 }
 
 function setToken() {
+  if(inputToken.value == undefined || inputToken.value == "" || inputToken.value == "undefined"){
+    alert("Invalid token!")
+    return;
+  }
   chrome.storage.local.set({ "Lichess-Token": inputToken.value });
   chrome.storage.local.set({ "IsTokenProvided": true });
   alert("token value =" +inputToken.value);
-  0
+  
 }
 
 function updateWinRange(blackRate) {
